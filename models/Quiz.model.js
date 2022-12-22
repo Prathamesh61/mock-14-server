42b6e9a90cc909cd0bbd8ea1de9a2c471999a70f
Prathamesh61 <rawoolprathamesh61@gmail.com>
2022-12-22 14:21:41 +0530

    mock-14

--- models/Quiz.model.js
+++ models/Quiz.model.js
@@ -5,8 +5,8 @@ const quizSchema = new mongoose.Schema({
   type: { type: String, require: true },
   difficulty: { type: String, require: true },
   question: { type: String, require: true },
-  correct: { type: String, require: true },
-  incorrect: { type: Array, require: true },
+  correct_answer: { type: String, require: true },
+  incorrect_answers: { type: Array, require: true },
 });
 
 const QuizModel = mongoose.model("quizQuestion", quizSchema);
