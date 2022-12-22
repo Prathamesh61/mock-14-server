6c7c2bcd6865d7365299883f14fdb5a1c6c1e35f
Prathamesh61 <rawoolprathamesh61@gmail.com>
2022-12-22 14:20:09 +0530

    mock-14

--- routes/quiz.route.js
+++ routes/quiz.route.js
@@ -9,6 +9,7 @@ quizRouter.get("/", async (req, res) => {
   const cat = req.query.cat;
   const diff = req.query.diff;
   const num = Number(req.query.num);
+  console.log(cat, diff, num);
   const questions = await QuizModel.find({ category: cat, difficulty: diff });
   questions.length = num || 0;
   console.log(questions.length);
